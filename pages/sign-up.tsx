702accae738a52b5b34cf769de495b7885296b5f
AdamAzuddin <05adamaz@gmail.com>
2023-04-09 16:38:16 +0800

    added register function

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useCallback, useState } from 'react';
 import { TextField, Button } from "@mui/material";
 import GoogleButton from "@/components/googleButton";
+import axios from 'axios';
 
 const SignUpPage = () => {
+
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post('/api/register', {
+        email,
+        username,
+        password
+      })
+    } catch (error){
+      console.log(error)
+    }
+  }, [email, username, password])
   return (
     <div className="flex flex-row justify-center items-center h-screen bg-zinc-900">
       <div className="h-120 p-6 bg-white rounded-lg shadow-lg grid grid-rows-3 gap-1 justify-center items-center">
@@ -40,6 +58,7 @@ const SignUpPage = () => {
           variant="contained"
           color="primary"
           className="text-white bg-blue-500 hover:bg-blue-600"
+          onClick={register}
         >
           Sign up
         </Button>
