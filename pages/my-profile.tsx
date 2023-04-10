0466b1d029497dae7587c01b74be3de85fbcadff
AdamAzuddin <05adamaz@gmail.com>
2023-04-10 13:19:35 +0800

    added things to my-profile page

--- pages/my-profile.tsx
+++ pages/my-profile.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import { Button } from "@mui/material";
+import { signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
+import Layout from "@/components/Layout";
+import Header from "@/components/Header";
 
 const MyProfile = () => {
-  return (
-    <div>My Profile</div>
-  )
-}
+  const { data, status } = useSession();
 
-export default MyProfile
+  const handleSignOut = async () => {
+    await signOut();
+  };
+
+  return (
+    <>
+      <Layout>
+        <h3 className="text-white text-4xl">Authorized user homepage</h3>
+        <div className="details">
+          <h5>{data?.user?.name}</h5>
+          <h5>{data?.user?.email}</h5>
+        </div>
+        <Button onClick={handleSignOut} variant="contained">
+          Sign out
+        </Button>
+      </Layout>
+    </>
+  );
+};
+
+export default MyProfile;
