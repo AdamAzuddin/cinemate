767c065bfc1007f936bf35bb4f3ceccd053538bf
AdamAzuddin <05adamaz@gmail.com>
2023-04-10 13:03:57 +0800

    added google icon

--- components/googleButton.tsx
+++ components/googleButton.tsx
@@ -1,4 +1,5 @@
 import { Button, ButtonProps } from '@mui/material';
+import { FcGoogle } from 'react-icons/fc';
 
 interface GoogleButtonProps extends ButtonProps {
   text: string
@@ -10,12 +11,12 @@ const GoogleButton = (props: GoogleButtonProps) => {
   return (
     <Button
     {...props}
-      variant="contained"
-      color="primary"
+      variant="outlined"
       disableRipple
       className="text-black bg-white hover:bg-gray-100"
     >
       {props.text} with Google
+      <FcGoogle size={32}/>
     </Button>
   );
 };
