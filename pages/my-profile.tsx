02604b8c6bf41e36ae0125089eb1b7f3d25a7ada
AdamAzuddin <05adamaz@gmail.com>
2023-04-11 11:07:40 +0800

    centered ui my-profile

--- pages/my-profile.tsx
+++ pages/my-profile.tsx
@@ -4,7 +4,6 @@ import { Button } from "@mui/material";
 import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
 import Layout from "@/components/Layout";
-import Header from "@/components/Header";
 
 const MyProfile = () => {
   const { data, status } = useSession();
@@ -16,14 +15,16 @@ const MyProfile = () => {
   return (
     <>
       <Layout>
-        <h3 className="text-white text-4xl">Authorized user homepage</h3>
-        <div className="details">
-          <h5>{data?.user?.name}</h5>
-          <h5>{data?.user?.email}</h5>
-        </div>
-        <Button onClick={handleSignOut} variant="contained">
-          Sign out
-        </Button>
+        <main className="container mx-auto text-center py-20">
+          <h3 className="text-white text-4xl">Authorized user homepage</h3>
+          <div className="details">
+            <h5 className="text-white">{data?.user?.name}</h5>
+            <h5 className="text-white">{data?.user?.email}</h5>
+          </div>
+          <Button onClick={handleSignOut} variant="contained">
+            Sign out
+          </Button>
+        </main>
       </Layout>
     </>
   );
