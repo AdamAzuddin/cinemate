a1aca38721973cba13b2b2db295bc778960eceae
AdamAzuddin <05adamaz@gmail.com>
2023-04-16 21:31:02 +0800

    readded guest and user page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,71 @@
+import Header from "@/components/Header";
+import Link from "next/link";
+import type { Session } from "next-auth";
 import Layout from "@/components/Layout";
-import {getSession, useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
+import { Button } from "@mui/material";
 import { NextPageContext } from "next";
-import Movies from "@/components/Movies";
-import Banner from "@/components/Banner";
-import useApi from "@/hooks/useApi";
-
-// if not signed in, cannot view home page
-export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context);
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/sign-in',
-        permanent: false,
-      }
-
-    };
-  }
-
-  return {
-    props: {},
-  };
-}
+import useCurrentUser from "@/hooks/useCurrentUser";
 
 export default function Home() {
+  const { data, status } = useSession();
+
+  const handleSignOut = async () => {
+    await signOut();
+  };
 
   return (
     <>
       <Layout>
-        <main className="container mx-auto  py-20">
-          <Banner/>
-          <section>
-            {/* Row*/}
-          </section>
+        <main>
+          {status === "authenticated" ? <User session={data} /> : <Guest />}
+          <Button onClick={handleSignOut} variant="contained">
+            Sign out
+          </Button>
         </main>
       </Layout>
     </>
+  );
+}
+
+//Guest
+function Guest() {
+  return (
+    <main className="container mx-auto text-center py-20">
+      <h3 className="text-white">Guest Home Page</h3>
+
+      <div className="justify-center flex">
+        <Link href="/sign-in" legacyBehavior>
+          <a className="text-white">Log In</a>
+        </Link>
+      </div>
+    </main>
+  );
+}
+
+//User
+type UserProps = {
+  session: Session | null;
+};
+function User({ session }: UserProps) {
+  const handleSignOut = async () => {
+    await signOut();
+  };
+  return (
+    <main className="container mx-auto text-center py-20">
+      <h3 className="text-white text-4xl">Authorized user homepage</h3>
+      <div className="details">
+        <h5>{session?.user?.name}</h5>
+        <h5>{session?.user?.email}</h5>
+      </div>
+      <div className="justify-center flex bg-blue-500">
+        <Link href="/profile" legacyBehavior>
+          <a className="text-white">Profile Page</a>
+        </Link>
+      </div>
+      <Button onClick={handleSignOut} variant="contained">
+        Sign out
+      </Button>
+    </main>
   );
 }
