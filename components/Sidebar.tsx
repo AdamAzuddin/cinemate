557dc37d6108f81e3091b1a79645079f8e8010b2
AdamAzuddin <05adamaz@gmail.com>
2023-04-17 22:03:24 +0800

    able to navigate to different pages from sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,6 +9,7 @@ import { ImExit } from "react-icons/im";
 import { HiUserCircle } from "react-icons/hi";
 import { IoMdMenu } from "react-icons/io";
 import { BsMegaphone } from "react-icons/bs";
+import Link from "next/link";
 
 interface Props {
   isSidebarOpen: boolean;
@@ -20,32 +21,46 @@ const Sidebar: React.FC<Props> = ({ isSidebarOpen, setIsSideBarOpen }) => {
     <div className={`h-screen bg-blue-600 px-2 flex flex-col`}>
       <IoMdMenu size={32} color="white" onClick={setIsSideBarOpen} />
       <div className="flex">
-        <HiUserCircle size={32} color="#898989" className="my-2" />
-        {isSidebarOpen && <span>Account</span>}
+        <Link href="/my-profile">
+          <HiUserCircle size={32} color="#898989" className="my-2" />
+          {isSidebarOpen && <span>Account</span>}
+        </Link>
       </div>
       <div className="flex">
-        <AiOutlineHome size={32} color="#898989" className="my-2" />
-        {isSidebarOpen && <span>Home</span>}
+        <Link href="/">
+          <AiOutlineHome size={32} color="#898989" className="my-2" />
+          {isSidebarOpen && <span>Home</span>}
+        </Link>
       </div>
       <div className="flex">
-        <BiCameraMovie size={32} color="#898989" className="my-2" />
-        {isSidebarOpen && <span>Movies</span>}
+        <Link href="/movies">
+          <BiCameraMovie size={32} color="#898989" className="my-2" />
+          {isSidebarOpen && <span>Movies</span>}
+        </Link>
       </div>
       <div className="flex">
-        <BiMoviePlay size={32} color="#898989" className="my-2" />
-        {isSidebarOpen && <span>Series</span>}
+        <Link href="/series">
+          <BiMoviePlay size={32} color="#898989" className="my-2" />
+          {isSidebarOpen && <span>Series</span>}
+        </Link>
       </div>
       <div className="flex">
-        <BsBookmarkPlus size={32} color="#898989" className="my-2" />
-        {isSidebarOpen && <span>My List</span>}
+        <Link href="/my-list">
+          <BsBookmarkPlus size={32} color="#898989" className="my-2" />
+          {isSidebarOpen && <span>My List</span>}
+        </Link>
       </div>
       <div className="flex">
-        <FaShapes size={32} color="#898989" className="my-2" />
-        {isSidebarOpen && <span>Genres</span>}
+        <Link href="/genre">
+          <FaShapes size={32} color="#898989" className="my-2" />
+          {isSidebarOpen && <span>Genres</span>}
+        </Link>
       </div>
       <div className="flex">
-        <BsMegaphone size={32} color="#898989" className="my-2" />
-        {isSidebarOpen && <span>Coming soon</span>}
+        <Link href="/coming-soon">
+          <BsMegaphone size={32} color="#898989" className="my-2" />
+          {isSidebarOpen && <span>Coming soon</span>}
+        </Link>
       </div>
       <div className="mt-auto">
         <div className="flex bottom-0 absolute">
---
+++ pages/coming-soon.tsx
@@ -0,0 +1,16 @@
+import Layout from '@/components/Layout'
+import React from 'react'
+
+const ComingSoon = () => {
+  return (
+    <>
+      <Layout>
+        <main className="container mx-auto text-center py-20">
+          <h1 className="text-white text-3xl">Coming soon</h1>
+        </main>
+      </Layout>
+    </>
+  )
+}
+
+export default ComingSoon
