76cc3dd7d66cec7fcda064559f88ef39d7a5ffdd
AdamAzuddin <05adamaz@gmail.com>
2023-04-17 17:49:58 +0800

    styled sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,52 +8,53 @@ import { FaShapes } from "react-icons/fa";
 import { ImExit } from "react-icons/im";
 import { HiUserCircle } from "react-icons/hi";
 import { IoMdMenu } from "react-icons/io";
+import { BsMegaphone } from "react-icons/bs";
 
 interface Props {
   isSidebarOpen: boolean;
-  setIsSideBarOpen: ()=> void;
+  setIsSideBarOpen: () => void;
 }
 
-const Sidebar: React.FC<Props> = ({isSidebarOpen, setIsSideBarOpen}) => {
+const Sidebar: React.FC<Props> = ({ isSidebarOpen, setIsSideBarOpen }) => {
   return (
     <div className={`h-screen bg-blue-600 px-2 flex flex-col`}>
-      <IoMdMenu size={32} color="white" onClick={setIsSideBarOpen}/>
+      <IoMdMenu size={32} color="white" onClick={setIsSideBarOpen} />
       <div className="flex">
-          <HiUserCircle size={32}/>
-          {isSidebarOpen && <span>John doe</span>}
+        <HiUserCircle size={32} color="#898989" className="my-2" />
+        {isSidebarOpen && <span>Account</span>}
       </div>
       <div className="flex">
-          <AiOutlineHome size={32}/>
-          {isSidebarOpen && <span>John doe</span>}
+        <AiOutlineHome size={32} color="#898989" className="my-2" />
+        {isSidebarOpen && <span>Home</span>}
       </div>
       <div className="flex">
-          <BiCameraMovie size={32}/>
-          {isSidebarOpen && <span>John doe</span>}
+        <BiCameraMovie size={32} color="#898989" className="my-2" />
+        {isSidebarOpen && <span>Movies</span>}
       </div>
       <div className="flex">
-          <BiMoviePlay  size={32}/>
-          {isSidebarOpen && <span>John doe</span>}
+        <BiMoviePlay size={32} color="#898989" className="my-2" />
+        {isSidebarOpen && <span>Series</span>}
       </div>
       <div className="flex">
-          <BsBookmarkPlus  size={32}/>
-          {isSidebarOpen && <span>John doe</span>}
+        <BsBookmarkPlus size={32} color="#898989" className="my-2" />
+        {isSidebarOpen && <span>My List</span>}
       </div>
       <div className="flex">
-          <FaShapes size={32}/>
-          {isSidebarOpen && <span>John doe</span>}
+        <FaShapes size={32} color="#898989" className="my-2" />
+        {isSidebarOpen && <span>Genres</span>}
       </div>
       <div className="flex">
-          <GrAnnounce size={32}/>
-          {isSidebarOpen && <span>John doe</span>}
+        <BsMegaphone size={32} color="#898989" className="my-2" />
+        {isSidebarOpen && <span>Coming soon</span>}
       </div>
       <div className="mt-auto">
-    <div className="flex bottom-0 absolute">
-      <div className="flex-grow"></div>
-      <div>
-        <ImExit size={32} />
+        <div className="flex bottom-0 absolute">
+          <div className="flex-grow"></div>
+          <div>
+            <ImExit size={32} />
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
     </div>
   );
 };
